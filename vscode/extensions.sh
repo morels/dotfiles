1044e2af853fa0343189509beace73c811ca2664
Luca Morelli <luca.morelli@ovalmoney.com>
2022-03-22 18:31:01 +0100

    feat: add initial content

---
+++ vscode/extensions.sh
@@ -0,0 +1,54 @@
+# List of VS Code extensions
+# The following content has been created automatically by running
+#
+# $ code --list-extensions | sed -e 's/^/code --install-extension /'
+#
+# Further info at: https://stackoverflow.com/questions/35773299/how-can-you-export-the-visual-studio-code-extension-list
+#
+code --install-extension Arjun.swagger-viewer
+code --install-extension attilabuti.vscode-mjml
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension csstools.postcss
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension DotJoshJohnson.xml
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension ecmel.vscode-html-css
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eg2.tslint
+code --install-extension hex-ci.stylelint-plus
+code --install-extension letrieu.expand-region
+code --install-extension liamhammett.inline-parameters
+code --install-extension mblode.twig-language-2
+code --install-extension mervin.markdown-formatter
+code --install-extension mikestead.dotenv
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-audio
+code --install-extension ms-vsliveshare.vsliveshare-pack
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension msjsdiag.vscode-react-native
+code --install-extension nhoizey.gremlins
+code --install-extension octref.vetur
+code --install-extension Orta.vscode-jest
+code --install-extension redhat.vscode-yaml
+code --install-extension silvenon.mdx
+code --install-extension stylelint.vscode-stylelint
+code --install-extension tommasov.hosts
+code --install-extension waderyan.gitblame
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension whatwedo.twig
+code --install-extension wmaurer.change-case
+code --install-extension Zignd.html-css-class-completion
